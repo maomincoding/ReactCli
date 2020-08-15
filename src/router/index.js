679b2ea477b1aaee7e8770148a4bf0c8ed04b097
maomincoding <17864296568@163.com>
2020-08-15 15:55:33 +0800

    2020.8.15 15:55

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,10 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 // import { Provider } from 'react-redux';
 // import store from '../store/index';
 import { Home, About, More} from './routes'
-import { Button } from 'antd';
 
 const APPRouter = () =>(
         <div>
             <Router>
-            <Button>按钮</Button>
                 <div>
                 <Route exact={true}  path="/" component={Home}/>
                 <Route exact={true}  path="/about" component={About}/>
